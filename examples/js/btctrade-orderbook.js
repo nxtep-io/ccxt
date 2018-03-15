7ba078467243afab29169f2ad358203462403cab
João Marcelo Brito <joaomarcelobrito01@gmail.com>
2018-03-15 18:00:00 -0300

    Fix: Typo correction in createOrder for btctrade

--- examples/js/btctrade-orderbook.js
+++ examples/js/btctrade-orderbook.js
@@ -1,7 +1,12 @@
 const ccxt      = require ('../../ccxt.js')
 
-const btcTrade = new ccxt.btctrade();
+const btcTrade = new ccxt.btctrade({apiKey: 'U2FsdGVkX19HiQ7RU/+Zh3osWQKx6uo3DmsS2DeMPbY='});
 
-btcTrade.fetchOrderBook('BTC/BRL')
+async function Test() {
+  const balance = await btcTrade.createOrder('BTC', 'market', 'buy', 0.00001);
+  console.log(balance);
+}
+
+Test()
   .then(e => console.log(e))
   .catch(e => console.log(e));
